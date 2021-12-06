f8f643fe22b77e99b8bef99581a1def70ca30164
Ayon Somaddar <89964596+Ayon06112002@users.noreply.github.com>
2021-12-06 19:46:43 +0530

    Add files via upload

---
+++ SHIVLING.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() 
+{int i,j; 
+for(j=26;j>=-52;j--){ 
+for(i=-39;i<=39;i++){ 
+if((abs(i)<22&&(abs(j-4)<2||abs(j+2)<2||abs(j+8)<2)||(abs(i)==22&&(j==4||j==-2||j==-8)))||(!((676*i*i+1521*j*j<=822557)||(abs(i)<35&&j<0)))) 
+cout<<" "; 
+else 
+cout<<"*";} 
+cout<<std::endl;} 
+return 0;}
